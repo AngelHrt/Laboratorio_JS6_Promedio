2cf7a47e8a089e1f92e14baf1a4f2208bd433965
AngelHrt <saiber798@gmail.com>
2024-06-18 18:48:11 -0500

    html y js final

--- script.js
+++ script.js
@@ -1,23 +1,87 @@
 class Alumno{
-	#codigo;
+	codigo;
+	nombre;
+	nota1;
+	nota2;
+	nota3;
+	nota4;
 
-	constructor(codigo){
-		this.#codigo = codigo;
+
+	constructor(codigo,nombre,nota1,nota2,nota3,nota4){
+		this.codigo = codigo;
+		this.nombre = nombre;
+		this.nota1 = parseFloat(nota1);
+        this.nota2 = parseFloat(nota2);
+        this.nota3 = parseFloat(nota3);
+        this.nota4 = parseFloat(nota4);
 	}
-	
+
 	promedio(){
-		return (nota1/2);
-	}
+		let promedio = (this.nota1*0.15)+(this.nota2*0.20)+(this.nota3*0.25)+(this.nota4*0.40);
+		return promedio;
 	
-	set setcodigo(codigo){
-		this.#codigo = codigo;
 	}
-	get getcodigo(){
-		return this.#codigo;
+	condicion (){
+		if (this.promedio() >= 12){
+			return "Aprobado";
+		}else{
+			return "Desaprobado";
+		}
+	}
+
+	obsequio(){
+		if (this.promedio() > 17){
+			return "se le regala una mochila"
+		}else{
+			return "no se le regala la mochila"
+		}
 	}
 }
+// const alum = new Alumno(123, "Fabian", 12, 20, 0, 0);
+// console.log(alum);
+// console.log(alum.promedio());
+// console.log(alum.condicion());
+// console.log(alum.obsequio());
 
-const alum1 = new Alumno('159');
-// alum1.codigo = 1563;
-alum1.setcodigo= 50;
-console.log(alum1.getcodigo);
+// $(document).ready(function() {
+// 	$('#miFormulario').submit(function(e) {
+// 		e.preventDefault();
+
+// 		const alumno = new Alumno(
+// 			$('#codigo').val(),
+// 			$('#nombre').val(),
+// 			$('#nota1').val(),
+// 			$('#nota2').val(),
+// 			$('#nota3').val(),
+// 			$('#nota4').val(),
+// 			);
+
+// 		const promedio = alumno.promedio();
+// 		const condicion = alumno.condicion();
+// 		const obsequio = alumno.obsequio();
+
+// 		alert(`El alumno ${alumno.nombre} con código ${alumno.codigo} a ${condicion} con una nota de ${promedio.toFixed(2)} y ${obsequio}`);
+// 	});
+// });
+
+
+$(document).ready(function() {
+	$('#miFormulario').submit(function(e) {
+		e.preventDefault();
+
+		const codigo = $('#codigo').val();
+		const nombre = $('#nombre').val();
+		const nota1 = $('#nota1').val();
+		const nota2 = $('#nota2').val();
+		const nota3 = $('#nota3').val();
+		const nota4 = $('#nota4').val();
+
+		const alumno = new Alumno(codigo, nombre, nota1, nota2, nota3, nota4);
+
+		const promedio = alumno.promedio();
+		const condicion = alumno.condicion();
+		const obsequio = alumno.obsequio();
+
+		alert(`El alumno ${alumno.nombre} con código ${alumno.codigo} a ${condicion} con una nota de ${promedio.toFixed(2)} y ${obsequio}`);
+	});
+});
